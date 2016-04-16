d057f77c3b9b9987e0ed868338f13e65b8943b29
Mark Onofrio <hello@markonofrio.com>
2016-04-15 22:02:07 -0400

    update with rename

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename');
 
-gulp.task('default', function(){
+gulp.task('default', ['rename'], function(){});
+
+gulp.task('rename', function(){
+  gulp.src('./style.css')
+  .pipe(rename("./mod-icons.scss"))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('scss', function(){
   gulp.src('./*.scss')
   .pipe(sass().on('error', sass.logError))
   .pipe(gulp.dest('./'));
