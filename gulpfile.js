271dadb8b174fb54e51b809db1e0d46db974252b
Mark Onofrio <hello@markonofrio.com>
2018-04-04 20:21:57 -0400

    updated icon list. added 3 new custom icons. Updated files for easier updates.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,16 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
     rename = require('gulp-rename');
 
-gulp.task('default', ['rename'], function(){});
+gulp.task('default', ['move','rename']);
+
+gulp.task('move', function(){
+  gulp.src('./icomoon/fonts/*')
+      .pipe(gulp.dest('./fonts'))
+});
 
 gulp.task('rename', function(){
-  gulp.src('./style.css')
+  gulp.src('./icomoon/style.css')
   .pipe(rename("./mod-icons.scss"))
   .pipe(gulp.dest('./'));
 });
 
-gulp.task('scss', function(){
-  gulp.src('./*.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./'));
-});
